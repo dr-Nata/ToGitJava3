659690ce2a038b54e3e162bf2e022984644bf3a5
dr-Nata <nberkunsky@gmail.com>
2022-12-21 19:03:47 +0200

    Initial commit - 1

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        new Main().run();
+
+
+    }
+
+    private void run() {
+        System.out.println("Hello world!");
+    }
+}
