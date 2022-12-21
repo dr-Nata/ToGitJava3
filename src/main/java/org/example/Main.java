6af45e159180ee9ef501b87c8670d16e92f9aa69
dr-Nata <37813403+dr-Nata@users.noreply.github.com>
2022-12-21 19:28:48 +0200

    Update Main.java
    System.out.printf("За %d  %s  вы заплатите %18.2f грн", n, name,cost*n);

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,7 +8,9 @@ public class Main {
     private void run3() {
         String name = "Shirt";
         double cost = 450.769204;
+        int n = 5;
         System.out.printf("Товар: %s; стоит: %18.2f грн",name,cost);
+        System.out.printf("За %d  %s  вы заплатите %18.2f грн", n, name,cost*n);
     }
 
     private void run2() {
